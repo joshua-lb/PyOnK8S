1798f0aed8f9f57f5385685d094b945cfeb24a58
joshua-lb <80673197+joshua-lb@users.noreply.github.com>
2021-07-26 13:21:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from random import randint
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/random")
+def random():
+    random_number = randint(1, 10)
+    return {'random': random_number}
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+# $ minikube service hello-python-service --url
